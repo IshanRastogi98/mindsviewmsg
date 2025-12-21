e4cacc0a956b82bc40352d17d308fd8c62dc4024
Ishan Rastogi <ishanrastogi987654321@gmail.com>
2025-12-22 03:26:20 +0530

    Fixed the CSP

--- next.config.ts
+++ next.config.ts
@@ -50,55 +50,88 @@ const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
   async headers() {
-    if (isDev)
-      return [
-        {
-          source: "/:path*",
-          headers: [
-            {
-              key: "X-Frame-Options",
-              value: "DENY",
-            },
-            {
-              key: "X-Content-Type-Options",
-              value: "nosniff",
-            },
-            {
-              key: "Referrer-Policy",
-              value: "strict-origin-when-cross-origin",
-            },
-            {
-              key: "Permissions-Policy",
-              value:
-                "camera=(), microphone=(), geolocation=(), browsing-topics=()",
-            },
-          ],
-        },
-      ];
+    // if (isDev)
+    //   return [
+    //     {
+    //       source: "/:path*",
+    //       headers: [
+    //         {
+    //           key: "X-Frame-Options",
+    //           value: "DENY",
+    //         },
+    //         {
+    //           key: "X-Content-Type-Options",
+    //           value: "nosniff",
+    //         },
+    //         {
+    //           key: "Referrer-Policy",
+    //           value: "strict-origin-when-cross-origin",
+    //         },
+    //         {
+    //           key: "Permissions-Policy",
+    //           value:
+    //             "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+    //         },
+    //       ],
+    //     },
+    //   ];
 
     return [
       {
         source: "/:path*",
         headers: [
+          // adden the strict Content Security Policy (CSP)
+          // to restrict the 'browser's operaytions over/from unknown targets
           {
-            // adden the strict Content Security Policy (CSP)
-            // to restrict the 'browser's operaytions over/from unknown targets
             key: "Content-Security-Policy",
             value: `
-              default-src 'self';
-              script-src 'self';
-              script-src-elem 'self';
-              style-src 'self' 'unsafe-inline';
-              style-src-elem 'self' 'unsafe-inline';
-              img-src 'self' data: blob:;
-              font-src 'self';
-              connect-src 'self';
-              worker-src 'self' blob:;
-              object-src 'none';
-              base-uri 'self';
-              frame-ancestors 'none';
-              upgrade-insecure-requests;
-            `
+    default-src 'self';
+
+    script-src
+      'self'
+      'unsafe-inline'
+      'unsafe-eval';
+
+    script-src-elem
+      'self'
+      'unsafe-inline';
+
+    style-src
+      'self'
+      'unsafe-inline';
+
+    style-src-elem
+      'self'
+      'unsafe-inline';
+
+    img-src
+      'self'
+      data:
+      blob:
+      https:;
+
+    font-src
+      'self'
+      data:;
+
+    connect-src
+      'self'
+      https:
+      wss:;
+
+    worker-src
+      'self'
+      blob:;
+
+    frame-src
+      'self'
+      https:;
+
+    object-src 'none';
+    base-uri 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+  `
               .replace(/\s{2,}/g, " ")
               .trim(),
           },
